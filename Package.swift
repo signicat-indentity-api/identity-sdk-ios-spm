20c8752f6ee1da3f5086d1b3f0fc7aa25ba08333
tmx <maplender@gmail.com>
2026-03-20 21:01:33 +0100

    3.7.1

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
        /// The binary XCFramework target
        .binaryTarget(
            name: "IdentitySDK",
-           url: "https://github.com/signicat-indentity-api/identity-sdk-ios/archive/refs/tags/v3.7.0.zip",
-           checksum: "d3e1d1895fa2fc407da7f3d1c00bccac52e8c4dc90cb74ce94b36bcb69b8e7ff"
+           url: "https://github.com/signicat-indentity-api/identity-sdk-ios/archive/refs/tags/v3.7.1.zip",
+           checksum: "3dbecbc7d10ce4aebb33040fe79b5b1c3271d2abcaee109aa4092ee372584526"
        )
     ]
 )
